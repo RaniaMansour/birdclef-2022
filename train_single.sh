0bd0fcd9697fc27f826eb93713dcaaf53c69f98d
Rania Helmy <rouna.rbb1@gmail.com>
2023-04-22 18:28:31 +0200

    Update train_single.sh

--- train_single.sh
+++ train_single.sh
@@ -8,6 +8,4 @@ DATA_DIR=$5
 PREFIX=$6
 RESUME=$7
 
-PYTHONPATH=.  python -u train_classifier.py --gpu $GPU  \
- --config configs/${CONFIG}.json  --workers 8 --test_every 1 \
- --val-dir $VAL_OUT_DIR  --prefix $PREFIX --fold $FOLD --freeze-epochs 0 --data-dir $DATA_DIR --resume weights/$RESUME
+PYTHONPATH=.  python -u train_classifier.py --gpu $GPU  --config configs/${CONFIG}.json  --workers 8 --test_every 1 --val-dir $VAL_OUT_DIR  --prefix $PREFIX --fold $FOLD --freeze-epochs 0 --data-dir $DATA_DIR --resume weights/$RESUME
